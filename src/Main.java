458b7df5bea195e8c6b3610bef074ae84d8a2be3
Rohan <Rohanpatel512>
2021-02-05 13:40:42 -0500

    Connect four game made with java

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+/**
+ *
+ * @author Rohan
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        // Starts of the game 
+        Game connectFour = new Game();
+        
+    }
+    
+}
